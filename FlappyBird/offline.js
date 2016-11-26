56c68f37df6763dd804f3c2448eeda7304d30671
Claudio Sampaio <claudio.sampaio@trdreams.com>
2016-11-26 14:33:10 -0400

    Jogo

---
+++ FlappyBird/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1480184781,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/bird-sheet0.png",
+		"images/bird-sheet1.png",
+		"images/background.png",
+		"images/ground.png",
+		"images/sprite-sheet0.png",
+		"images/pipetop-sheet0.png",
+		"images/pipedown-sheet0.png",
+		"images/times-sheet0.png",
+		"images/times-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
